c4930256b5a6165a3fceb0613362da0da9372c15
presenteniv <158219661+presenteniv@users.noreply.github.com>
2024-02-06 14:06:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
         numeroEstrelas = parseInt(numeroEstrelas);
 
         if (numeroEstrelas === 50) {
-            document.getElementById('resultado').innerHTML = '<img src="imagem-sucesso.jpg" alt="easter egg"><p>entãokkkkk, primeiramente eu ia te dar isso daí e a jarra, mas eu achei mt exagerado e que não fazia mt sentido, então fiquei pra mim e nem sei oq vou fazer com isso! alem disso acho q deve ter sido chato contar todas essas estrelas!!</p>';
+            document.getElementById('resultado').innerHTML = '<img src="imagem-sucesso.jpeg" alt="easter egg"><p>entãokkkkk, primeiramente eu ia te dar isso daí e a jarra, mas eu achei mt exagerado e que não fazia mt sentido, então fiquei pra mim e nem sei oq vou fazer com isso! alem disso acho q deve ter sido chato contar todas essas estrelas!!</p>';
         } else {
             document.getElementById('resultado').innerHTML = '<p>passou perto!</p>';
         }
