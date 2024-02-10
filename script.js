4ae72d06efeabb0b8060e75c5c53307457b941a9
presenteniv <158219661+presenteniv@users.noreply.github.com>
2024-02-09 23:30:33 -0300

    acabou.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function() {
         var numeroEstrelas = document.getElementById('starsInput').value;
         numeroEstrelas = parseInt(numeroEstrelas);
 
-        if (numeroEstrelas === 50) {
+        if (numeroEstrelas === 134) {
             document.getElementById('resultado').innerHTML = '<img src="imagem-sucesso.jpeg" alt="easter egg"><p>entãokkkkk, primeiramente eu ia te dar isso daí e a jarra, mas eu achei mt exagerado e que não fazia mt sentido, então fiquei pra mim e nem sei oq vou fazer com isso! alem disso acho q deve ter sido chato contar todas essas estrelas!!</p>';
         } else {
             document.getElementById('resultado').innerHTML = '<p>passou perto!</p>';
